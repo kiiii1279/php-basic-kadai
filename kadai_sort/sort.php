1e131c91b5494d59c9ac5786c6cdf4d28524ced4
kiiii1279 <yuki105202305@gmail.com>
2025-06-16 22:26:14 +0900

    Add files via upload

---
+++ kadai_sort/sort.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP基礎編</title>
+</head>
+<body>
+    <p>
+        <?php
+        function sort_2way($array, $order){
+
+        if ($order){
+        echo '昇順にソートします<br>';
+        
+       }else{
+          echo '降順にソートします<br>'; 
+          
+        
+        }
+      
+        } 
+
+        sort($array);
+        foreach($array as $nums) {
+          echo $nums . "<br>"; 
+        }
+
+        rsort($array);
+        foreach($array as $nums) {
+
+        echo $nums. "<br>";
+        }
+
+        $nums = [15, 4, 18, 23, 10];
+
+       
+    
+
+        ?>
+    </p>
+    
+    
+</body>
+</html>
