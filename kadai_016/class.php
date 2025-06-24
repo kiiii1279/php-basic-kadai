43ed9c057b520552c1b376cf4a4ccd26c07454fb
kiiii1279 <yuki105202305@gmail.com>
2025-06-24 21:05:07 +0900

    Add files via upload

--- kadai_016/class.php
+++ kadai_016/class.php
@@ -10,30 +10,59 @@
     <?php
     class Food{
 
-        public $name;
-        public $price;
+        private $name;
+        private $price;
 
-        public function show_price(string $price){
-            $this->price = $price;
-
+        public function __construct(string $name, int $price){
+            $this -> name = $name;
+            $this -> price = $price;   
         }
-        
+
+
+        public function show_price(){
+            echo $this ->price . '<br>';
+        }
     }
+        
     ?>
     </p>
+    
     <p>
         <?php
 
         class Animal {
 
-            public $name;
-            public $height;
-            public $weight;
+            private $name;
+            private $height;
+            private $weight;
 
-            public function show_height(string $height) {
-                $this->height = $height;
+            public function __construct(string $name, int $height, int $weight) {
+            $this ->name = $name;
+            $this ->height= $height;
+            $this ->weight= $weight;
             }
+
+            public function show_height(){
+                echo $this->height . '<br>';
+            }
+
+
         }
+
+
+        $potato= new Food ('potato', 250); 
+        $dog = new Animal ('dog', 60, 5000);
+
+        print_r($potato);
+        echo '<br>';
+        print_r($dog);
+        echo '<br>';
+
+        $potato->show_price(250);
+        $dog->show_height(60);
+
+
+
 
         ?>
     </p>
